23d2cb6a101b5c9b0f1b1257fc59f93cad8728b8
SurmDen <surmanidzedenis609@gmail.com>
2025-10-02 16:06:40 +0300

    added domain models

---
+++ MoneyTrackSolution/MoneyTrack.Domain/Models/Wallet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTrack.Domain.Models
+{
+    public class Wallet
+    {
+        public Guid Id { get; set; }
+
+        public string WalletName { get; set; } = string.Empty;
+
+        public Currency Currency { get; set; }
+
+        public decimal InitialBalance { get; set; }
+
+        public List<Transaction> Transactions { get; set; }
+    }
+
+    public enum Currency
+    {
+        USD,
+        RUB,
+        EUR
+    }
+}
